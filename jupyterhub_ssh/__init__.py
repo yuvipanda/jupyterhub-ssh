025529aed10c9c82224ce677b2cf0947dadc42da
YuviPanda <yuvipanda@gmail.com>
2020-10-18 00:44:00 +0530

    Accept JupyterHub tokens as auth
    - Consolidate processing in SSHServer class - we need it anyway
      to process auth, might as well also use it for session management.
      This also lets us do direct TCP connections in the future if
      needed
    - Talk to the hub API to get server URL info, rather than hardcode
      my own URL. This should also be configurable later on.
    - Reduce timeout for proxying content back / forth. This should be
      made full duplex, but until then...
    - Actually validate that the token is valid before accepting the
      connection. We do this by talking to the hub API and making
      sure we get a 200 response.

--- jupyterhub_ssh/__init__.py
+++ jupyterhub_ssh/__init__.py
@@ -4,15 +4,11 @@ import logging
 import websockets
 import json
 from yarl import URL
-from functools import partial
 
 from aiohttp import ClientSession
 
 
-async def create_terminado(
-        session: ClientSession,
-        notebook_url: URL,
-        token: str):
+async def create_terminado(session: ClientSession, notebook_url: URL, token: str):
     """
     Create a Terminal, returning websocket path
     """
@@ -26,57 +22,71 @@ async def create_terminado(
     return socket_url.with_scheme('wss' if notebook_secure else 'ws')
 
 
-TOKEN = ''
-NOTEBOOK_URL = 'https://datahub.berkeley.edu/user/yuvipanda/'
-
+HUB_URL = URL('https://datahub.berkeley.edu')
 
 class MySSHServer(asyncssh.SSHServer):
     def password_auth_supported(self):
         return True
 
-    def validate_password(self, username, password):
-        return username == password
+    async def validate_password(self, username, token):
+        self.username = username
+        self.token = token
 
+        headers = {
+            'Authorization': f'token {token}'
+        }
+        async with ClientSession(headers=headers) as session:
+            async with session.get(HUB_URL / 'hub/api/users' / username) as resp:
+                if resp.status != 200:
+                    return False
+                user = await resp.json()
+                # URLs will have preceding slash, but yarl forbids those
+                self.notebook_url = HUB_URL / user['servers']['']['url'][1:]
+                return True
 
-async def handle_client(process):
-    headers = {
-        'Authorization': f'token {TOKEN}'
-    }
+    async def _handle_client(self, stdin, stdout, stderr):
+        """
+        Handle a single ssh client
+        """
+        headers = {
+            'Authorization': f'token {self.token}'
+        }
 
-    async with ClientSession(headers=headers) as client:
-        ws_url = await create_terminado(client, URL(NOTEBOOK_URL), TOKEN)
-        print(ws_url)
+        async with ClientSession(headers=headers) as client:
+            ws_url = await create_terminado(client, self.notebook_url, self.token)
 
-    async with websockets.connect(str(ws_url), extra_headers=headers) as ws:
-        while not process.stdin.at_eof():
-            try:
-                data = await asyncio.wait_for(process.stdin.read(4096), 0.1)
-                print('in', data)
-                await ws.send(json.dumps(['stdin', data]))
-            except asyncio.TimeoutError:
-                pass
-            except asyncssh.misc.TerminalSizeChanged:
-                pass
-            except asyncssh.misc.BreakReceived:
-                pass
+        async with websockets.connect(str(ws_url), extra_headers=headers) as ws:
+            # FIXME: Make this *actually* full duplex!
+            # FIXME: This is terrible!
+            while not stdin.at_eof():
+                try:
+                    data = await asyncio.wait_for(stdin.read(4096), 0.01)
+                    await ws.send(json.dumps(['stdin', data]))
+                except asyncio.TimeoutError:
+                    pass
+                except asyncssh.misc.TerminalSizeChanged:
+                    pass
+                except asyncssh.misc.BreakReceived:
+                    pass
 
-            try:
-                in_data = json.loads(await asyncio.wait_for(ws.recv(), 0.1))
-                print('stdout', in_data)
-                if in_data[0] == 'stdout':
-                    process.stdout.write(in_data[1])
-                    await process.stdout.drain()
-                elif in_data[0] == 'stderr':
-                    stderr.write(in_data[1])
-                    await process.stderr.drain()
-            except asyncio.TimeoutError:
-                pass
+                try:
+                    in_data = json.loads(await asyncio.wait_for(ws.recv(), 0.01))
+                    if in_data[0] == 'stdout':
+                        stdout.write(in_data[1])
+                        await stdout.drain()
+                    elif in_data[0] == 'stderr':
+                        stderr.write(in_data[1])
+                        await stderr.drain()
+                except asyncio.TimeoutError:
+                    pass
+
+    def session_requested(self):
+        return self._handle_client
 
 async def start_server():
     await asyncssh.listen(
         host='',
         port=8022,
-        process_factory=handle_client,
         server_factory=MySSHServer,
         line_editor=False,
         server_host_keys=['ssh_host_key']
