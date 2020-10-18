2ffe2cbd5320c94b0c1e3e3cf49569900c30f85b
YuviPanda <yuvipanda@gmail.com>
2020-10-18 12:10:11 +0530

    Move terminado actions to a class
    There's enough here for it to be a real class, instead
    of just a bunch of functions. Should make unit testing
    against a non-jupyterhub notebook URL easier as well.
    
    We also pass terminal size info changes, but *only* when
    terminal size changes. We should do that on start too.
    
    Ref #5

--- jupyterhub_ssh/__init__.py
+++ jupyterhub_ssh/__init__.py
@@ -4,22 +4,63 @@ import logging
 import websockets
 import json
 from yarl import URL
+from contextlib import asynccontextmanager
 
 from aiohttp import ClientSession
 
+class Terminado:
+    @classmethod
+    async def create(cls, session, notebook_url, token):
+        notebook_secure = notebook_url.scheme == 'https'
 
-async def create_terminado(session: ClientSession, notebook_url: URL, token: str):
-    """
-    Create a Terminal, returning websocket path
-    """
-    notebook_secure = notebook_url.scheme == 'https'
+        create_url = notebook_url / "api/terminals"
+        async with session.post(create_url) as resp:
+            data = await resp.json()
+        terminal_name = data['name']
+        socket_url = notebook_url / 'terminals/websocket' / terminal_name
+        return cls(
+            notebook_url=notebook_url,
+            token=token,
+            ws_url=socket_url.with_scheme('wss' if notebook_secure else 'ws')
+        )
 
-    create_url = notebook_url / "api/terminals"
-    async with session.post(create_url) as resp:
-        data = await resp.json()
-    terminal_name = data['name']
-    socket_url = notebook_url / 'terminals/websocket' / terminal_name
-    return socket_url.with_scheme('wss' if notebook_secure else 'ws')
+    def __init__(self, notebook_url, token, ws_url):
+        self.notebook_url = notebook_url
+        self.token = token
+        self.ws_url = ws_url
+        self.ws = None
+
+    @asynccontextmanager
+    async def connect(self):
+        headers = {
+            'Authorization': f'token {self.token}'
+        }
+        async with websockets.connect(str(self.ws_url), extra_headers=headers) as ws:
+            self.ws = ws
+            yield
+        self.ws = None
+
+    def send(self, data):
+        """
+        Send given data to terminado socket
+
+        data should be a list of strings
+        """
+        return self.ws.send(json.dumps(data))
+
+    def send_stdin(self, data):
+        """
+        Send data to stdin of terminado
+
+        data should be a string
+        """
+        return self.send(['stdin', data])
+
+    def set_size(self, rows, cols):
+        """
+        Set terminado's terminal cols / rows size
+        """
+        return self.send(['set_size', rows, cols])
 
 
 HUB_URL = URL('https://datahub.berkeley.edu')
@@ -53,24 +94,24 @@ class MySSHServer(asyncssh.SSHServer):
         }
 
         async with ClientSession(headers=headers) as client:
-            ws_url = await create_terminado(client, self.notebook_url, self.token)
+            terminado = await Terminado.create(client, self.notebook_url, self.token)
 
-        async with websockets.connect(str(ws_url), extra_headers=headers) as ws:
+        async with terminado.connect():
             # FIXME: Make this *actually* full duplex!
             # FIXME: This is terrible!
             while not stdin.at_eof():
                 try:
                     data = await asyncio.wait_for(stdin.read(4096), 0.01)
-                    await ws.send(json.dumps(['stdin', data]))
+                    await terminado.send_stdin(data)
                 except asyncio.TimeoutError:
                     pass
-                except asyncssh.misc.TerminalSizeChanged:
-                    pass
+                except asyncssh.misc.TerminalSizeChanged as e:
+                    await terminado.set_size(e.height, e.width)
                 except asyncssh.misc.BreakReceived:
                     pass
 
                 try:
-                    in_data = json.loads(await asyncio.wait_for(ws.recv(), 0.01))
+                    in_data = json.loads(await asyncio.wait_for(terminado.ws.recv(), 0.01))
                     if in_data[0] == 'stdout':
                         stdout.write(in_data[1])
                         await stdout.drain()
