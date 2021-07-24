8397af6b8d56b7879db89aa934057a0ac0294517
Erik Sundell <erik.i.sundell@gmail.com>
2021-07-24 18:15:22 +0200

    helm chart: add general config passthrough ssh.config

--- helm-chart/images/jupyterhub-ssh/jupyterhub_ssh_config.py
+++ helm-chart/images/jupyterhub-ssh/jupyterhub_ssh_config.py
@@ -4,6 +4,9 @@ yaml = YAML()
 with open("/etc/jupyterhub-ssh/config/values.yaml") as f:
     config = yaml.load(f)
 
-c.JupyterHubSSH.host_key_path = "/etc/jupyterhub-ssh/config/hostKey"
+# FIXME: help this config migrate to ssh.config as well
 c.JupyterHubSSH.hub_url = config["hubUrl"]
-c.JupyterHubSSH.debug = True
+
+# load generic configuration
+for app, cfg in config["ssh"]["config"].items():
+    c[app].update(cfg)
